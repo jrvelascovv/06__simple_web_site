a0f75317e12d088a168314fe7b3a5453f6854559
José Ramón Velasco Vivas <jrvelasco@radiumtec.com>
2025-08-07 18:56:49 -0400

    Tersificación

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,7 @@
 (function () {
   
   const savedMode = localStorage.getItem('colorMode');
-  console.log(savedMode);
+  // console.log(savedMode);
   const darkModeMQ = window.matchMedia('(prefers-color-scheme: dark)');
   const body = document.getElementsByTagName('body')[0];
   const darkToggle = document.getElementById('dark');
@@ -9,12 +9,12 @@
 
   if (savedMode) {
     // colorMode: úsalo
-    console.log('usando colorMode');
+    // console.log('usando colorMode');
     body.classList = savedMode;
     savedMode === "dark" ? darkToggle.click() : lightToggle.click();
   } else if (darkModeMQ.matches) {
     // Tema general: aplica dark mode
-    console.log('usando System pref');
+    // console.log('usando System pref');
     body.classList = 'dark';
     darkToggle.click();
   } else {
