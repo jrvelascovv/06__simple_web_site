8b0e77b70586f3177b2b73c0e33f1b2b66453903
José Ramón Velasco Vivas <jrvelasco@radiumtec.com>
2025-08-07 18:48:00 -0400

    Toggle mode   -Funcionalidad de cambio dark / light     Usa preferencias del sistema hasta que el usuario cambia el modo. Luego, siempre se usa el modo guardado.

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,39 @@
-console.log("working");
+(function () {
+  
+  const savedMode = localStorage.getItem('colorMode');
+  console.log(savedMode);
+  const darkModeMQ = window.matchMedia('(prefers-color-scheme: dark)');
+  const body = document.getElementsByTagName('body')[0];
+  const darkToggle = document.getElementById('dark');
+  const lightToggle = document.getElementById('light');
+
+  if (savedMode) {
+    // colorMode: úsalo
+    console.log('usando colorMode');
+    body.classList = savedMode;
+    savedMode === "dark" ? darkToggle.click() : lightToggle.click();
+  } else if (darkModeMQ.matches) {
+    // Tema general: aplica dark mode
+    console.log('usando System pref');
+    body.classList = 'dark';
+    darkToggle.click();
+  } else {
+    body.classList = 'light';
+    lightToggle.click();
+  }
+
+  darkToggle.addEventListener('click', () => {
+    setAndSaveMode("dark");
+  });
+  lightToggle.addEventListener('click', () => {
+    setAndSaveMode("light");
+  });
+
+  const setAndSaveMode = mode => {
+    body.classList = mode;
+    localStorage.setItem('colorMode', mode);
+  };
+
+})();
+
+
